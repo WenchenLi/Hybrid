ebc328660ddff376c88d7e1cd0721f604eac82be
WenchenLi <largemaths@gmail.com>
2015-08-28 10:41:51 -0400

    saveMat to external storage, need to test.

--- app/src/main/java/com/ubicomp/hybrid/ImageProcessor.java
+++ app/src/main/java/com/ubicomp/hybrid/ImageProcessor.java
@@ -4,6 +4,8 @@ package com.ubicomp.hybrid;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.util.Log;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -11,12 +13,15 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+
 import static org.opencv.core.Core.absdiff;
 import static org.opencv.core.Core.add;
 /**
  * Created by ave on 8/27/15.
  */
 public class ImageProcessor {
+    static final String TAG = "ImageProcessor";
 
     static Mat Hybridize(Mat far, Mat close){
         Mat highFreq = new Mat();
@@ -44,8 +49,33 @@ public class ImageProcessor {
         Mat close = new Mat();
         Utils.bitmapToMat(BitmapFactory.decodeResource(context.getResources(), R.drawable.test1),far);
         Utils.bitmapToMat(BitmapFactory.decodeResource(context.getResources(), R.drawable.test2),close);
-        Bitmap bm = BitmapFactory.decodeResource(context.getResources(),R.drawable.test2);
+        Bitmap bm = BitmapFactory.decodeResource(context.getResources(), R.drawable.test2);
         Utils.matToBitmap(ImageProcessor.Hybridize(far, close),bm);
         return bm;
+    }
+    static void SaveImage (Mat mat,String filename) {
+        Mat mIntermediateMat = new Mat();
+
+        Imgproc.cvtColor(mat, mIntermediateMat, Imgproc.COLOR_RGBA2BGR, 3);
+
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File file = new File(path, filename);
+
+        Boolean bool = null;
+        filename = file.toString();
+        bool = Imgcodecs.imwrite(filename, mIntermediateMat);
+
+        if (bool == true)
+            Log.d(TAG, "SUCCESS writing image to external storage");
+        else
+            Log.d(TAG, "Fail writing image to external storage");
+    }
+    static Mat LoadImage(String filename) {
+        Mat mIntermediateMat = new Mat();
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File file = new File(path, filename);
+        filename = file.toString();
+        mIntermediateMat = Imgcodecs.imread(filename);
+        return mIntermediateMat;
     }
 }
